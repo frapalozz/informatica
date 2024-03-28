550c30182a4656b863357317e37dd741d63313b7
Francesco Palozzi <palozzifrancesco@libero.it>
2024-03-28 18:05:25 +0100

    font fix - 5

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,13 @@ import Container from "./components/Container"
 export default function App() {
 
   return (
-    <div className="font-mono">
-      <main className="flex flex-col w-screen justify-center items-center font-montserrat dark:bg-stone-950 bg-stone-100 min-h-dvh">
-        <Navbar />
+    <main className="flex flex-col w-screen justify-center items-center font-montserrat dark:bg-stone-950 bg-stone-100 min-h-dvh">
+      <Navbar />
 
-        <Container />
+      <Container />
 
-        <Footer />
-      </main>
-    </div>
-    
+      <Footer />
+    </main>
   )
 }
 
