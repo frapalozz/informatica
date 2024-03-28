e594eb72ab5ca04b46ce667913f0c01c1e881141
Francesco Palozzi <palozzifrancesco@libero.it>
2024-03-28 17:46:45 +0100

    fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 export default function Hero() {
     return(
         <>
-            <section className="max-w-screen-2xl w-screen center px-3.5 md:px-4 lg:px-6 xl:px-8 mt-20 flex flex-col items-center xl:mt-55 lg:mt-48 md:mt-40 mt-36">
+            <section className="max-w-screen-2xl w-screen center px-3.5 md:px-4 lg:px-6 xl:px-8 flex flex-col items-center xl:mt-55 lg:mt-48 md:mt-40 mt-36">
 
                 <h1 className="dark:text-zinc-400 text-zinc-600 text-center text-5xl lg:text-6xl font-extrabold max-w-2xl lg:max-w-4xl lg:tracking-wide">Tutto ciò che serve agli <span className="dark:text-zinc-200 text-zinc-950">studenti informatici</span> in un luogo solo</h1>
 
