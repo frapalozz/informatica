0303293a62528e58f1aa6c3a4b1e02161de511c8
Francesco Palozzi <palozzifrancesco@libero.it>
2024-03-28 18:23:39 +0100

    fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ export default function Hero() {
         <>
             <section className="max-w-screen-2xl w-screen center px-3.5 md:px-4 lg:px-6 xl:px-8 flex flex-col items-center xl:mt-55 lg:mt-48 md:mt-40 mt-36">
 
-                <h1 className="dark:text-zinc-400 text-zinc-600 lg:min-h-44 text-center text-5xl lg:text-6xl font-extrabold max-w-2xl lg:max-w-4xl lg:tracking-wide">Tutto ciò che serve agli <span className="dark:text-zinc-200 text-zinc-950">studenti informatici</span> in un luogo solo</h1>
+                <h1 className="dark:text-zinc-400 text-zinc-600 md:min-h-[180px] text-center text-5xl lg:text-6xl font-extrabold max-w-2xl lg:max-w-4xl lg:tracking-wide">Tutto ciò che serve agli <span className="dark:text-zinc-200 text-zinc-950">studenti informatici</span> in un luogo solo</h1>
 
                 <p className="dark:text-zinc-400 text-zinc-600 min-h-6 text-sm md:text-base font-medium mt-5">Aggiungi, Modifica, Migliora</p>
 
