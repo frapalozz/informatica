dcfe9723efd859bbbf376ab08e30ebacedb19404
Francesco Palozzi <palozzifrancesco@libero.it>
2024-07-12 12:36:44 +0200

    update

--- src/app/2anno/page.tsx
+++ src/app/2anno/page.tsx
@@ -3,6 +3,13 @@ import { Metadata } from "next"
 
 export const metadata: Metadata = {
     title: '2° Anno',
+    openGraph: {
+        title: "2° Anno",
+        description: "Tutto ciò che serve agli studenti informatici in un luogo solo",
+        images: {
+            url: "https://art.pixilart.com/c562a069cbc4da4.png",
+        }
+    }
 }
 
 export default function Sanno(){
