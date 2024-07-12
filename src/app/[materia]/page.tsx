1aeea0ad39da6d490d4b26c7d6226ee5747ed13c
Francesco Palozzi <palozzifrancesco@libero.it>
2024-07-12 11:49:28 +0200

    fix

--- src/app/[materia]/page.tsx
+++ src/app/[materia]/page.tsx
@@ -28,6 +28,6 @@ export default async function MateriaPage({ params }: {params: {materia: string}
     const datiMateria = data.dati.filter((item: any) => item.materia == params.materia);
 
     return(
-        <Materia key={datiMateria[0].materia} immagine={datiMateria[0].immagine} materia={datiMateria[0].materia} descrizione={datiMateria[0].descrizione} webex={datiMateria[0].webex} materiale={datiMateria[0].materiale} registrazioni={datiMateria[0].registrazioni} prove={datiMateria[0].prove} extraName={datiMateria[0].extraName} extra={datiMateria[0].extra} extraLink={datiMateria[0].extraLink} quantity={datiMateria[0].quantity}/>
+        <Materia key={datiMateria[0].materia} immagine={datiMateria[0].immagine} materia={datiMateria[0]._comment} descrizione={datiMateria[0].descrizione} webex={datiMateria[0].webex} materiale={datiMateria[0].materiale} registrazioni={datiMateria[0].registrazioni} prove={datiMateria[0].prove} extraName={datiMateria[0].extraName} extra={datiMateria[0].extra} extraLink={datiMateria[0].extraLink} quantity={datiMateria[0].quantity}/>
     )
 }
--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link"
 
 export default function Navbar() {
     return(
@@ -5,13 +6,13 @@ export default function Navbar() {
 
             <nav className="flex flex-row justify-between items-center w-full h-full xl:mt-9 lg:mt-7 md:mt-5 mt-4">
                 <div>
-                    <a aria-label="Home" href="https://frapalozzinformatica.netlify.app/" className="text-white flex flex-row justify-center items-center">
+                    <Link aria-label="Home" href="/" className="text-white flex flex-row justify-center items-center">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-12 h-12 stroke-black dark:stroke-white">
                             <path strokeLinecap="round" strokeLinejoin="round" d="m6.75 7.5 3 2.25-3 2.25m4.5 0h3m-9 8.25h13.5A2.25 2.25 0 0 0 21 18V6a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 6v12a2.25 2.25 0 0 0 2.25 2.25Z" />
                         </svg>
 
                         <h1 className="xl:text-4xl lg:text-3xl text-center lg:font-bold lg:inline hidden dark:text-white text-black">INFORMATICA</h1>
-                    </a>
+                    </Link>
                 </div>
 
                 <div>
