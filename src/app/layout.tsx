e7a7ef388ddc7a3bf67d8a22fc85be1df3fef3ef
Francesco Palozzi <99217899+frapalozz@users.noreply.github.com>
2024-07-30 16:42:13 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,7 +43,6 @@ export default function RootLayout({
           {children}
         <Footer></Footer>
         <script async defer src="https://scripts.simpleanalyticscdn.com/latest.js"></script>
-        <noscript><img src="https://queue.simpleanalyticscdn.com/noscript.gif" alt="" referrerPolicy="no-referrer-when-downgrade" /></noscript>
       </body>
     </html>
   );
