feec5568ceca9715f7da0518bc727678a4815682
Francesco Palozzi <palozzifrancesco@libero.it>
2024-08-03 20:03:02 +0200

    update metadata

--- src/app/risorsa/page.tsx
+++ src/app/risorsa/page.tsx
@@ -1,6 +1,18 @@
 "use client"
 
+import { Metadata } from "next";
 import { useState } from "react";
+
+export const metadata: Metadata = {
+    title: 'Aggiungi Risorsa',
+    openGraph: {
+        title: "Aggiungi Risorsa",
+        description: "Tutto ciò che serve agli studenti informatici in un luogo solo",
+        images: {
+            url: "https://art.pixilart.com/c562a069cbc4da4.png",
+        }
+    }
+}
 
 export default function Risorsa() {
 
