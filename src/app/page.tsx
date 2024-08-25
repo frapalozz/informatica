5015aa14e206a6d202764b4fcc7cb025cd052e22
Francesco Palozzi <palozzifrancesco@libero.it>
2024-08-25 13:38:44 +0200

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import CardGrid from "./components/cardgrid";
+import Popup from "./components/Popup";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center w-full">
-      <CardGrid anno="1"></CardGrid>
-    </main>
+    <>
+      <Popup />
+      <main className="flex min-h-screen flex-col items-center justify-center w-full">
+        <CardGrid anno="1"></CardGrid>
+      </main>
+    </>
+    
   );
 }
