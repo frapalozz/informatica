54fac9d0ee3cba5c8ab514b78234ed8ad1a2b74e
Francesco Palozzi <99217899+frapalozz@users.noreply.github.com>
2024-12-06 11:56:17 +0100

    Update internet

--- src/dati/data.ts
+++ src/dati/data.ts
@@ -184,11 +184,11 @@ export const dati =[
 
         webex: "https://unicam.webex.com/meet/fausto.marcantoni",
 
-        extraName: ["Appunti", "Classroom Tut. 23/24"],
-        extra: ["Notion", "m3nfww5"],
-        extraLink: ["https://francescopalozzi.notion.site/Appunti-Internet-10c4f1c8724e8064ad7bf9b17c7ca846?pvs=4", "https://classroom.google.com/u/2/c/NjQ0MTI5MjM3MzU5"],
+        extraName: ["Appunti", "Classroom", "Classroom Tut. 23/24"],
+        extra: ["Notion", "t6ki77a", "m3nfww5"],
+        extraLink: ["https://francescopalozzi.notion.site/Appunti-Internet-10c4f1c8724e8064ad7bf9b17c7ca846?pvs=4", "https://classroom.google.com/u/1/c/NzM1ODg1MDY4NjM4", "https://classroom.google.com/u/2/c/NjQ0MTI5MjM3MzU5"],
 
-        aggiornamento: "10/24/2024"
+        aggiornamento: "12/06/2024"
     }, 
     {
         "_comment": "Sistemi Operativi",
