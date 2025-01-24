f824d21687bc18406f0c717cd8ec5e59227169a4
Francesco Palozzi <99217899+frapalozz@users.noreply.github.com>
2025-01-24 08:48:14 +0100

    Update data.ts

--- src/dati/data.ts
+++ src/dati/data.ts
@@ -15,7 +15,7 @@ export const dati =[
 
         webex: "https://unicam.webex.com/meet/alessandro.dellacorte",
 
-        extraName: ["Pass. Classroom", "Formulario", "Altre Prove],
+        extraName: ["Pass. Classroom", "Formulario", "Altre Prove"],
         extra: ["POYLYUV", "Notion", "Notion"],
         extraLink: ["https://classroom.google.com/u/7/c/NDAyNjAxMTEzMDE0", "https://francescopalozzi.notion.site/Formulario-Discreta-ae0a264c560142c0941f35e896fc7016?pvs=4", "https://francescopalozzi.notion.site/Altre-Prove-1854f1c8724e807ebcfdebc61a850242?pvs=4"],
         
