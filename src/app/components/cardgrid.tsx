6f52c913f8aec79c86ac5e221f585c37da96ce1f
Francesco Palozzi <99217899+frapalozz@users.noreply.github.com>
2025-08-22 15:25:55 +0200

    fix accessability: add label to input

--- src/app/components/cardgrid.tsx
+++ src/app/components/cardgrid.tsx
@@ -26,8 +26,8 @@ export default function CardGrid( {anno}: any) {
         <section className="relative max-w-screen-2xl w-full center px-3.5 md:px-4 lg:px-6 xl:px-8 flex flex-col items-center mt-2 z-[60]">
 
             <div className="flex flex-col items-center justify-center font-medium text-sm">
-                <p className="dark:text-white text-black">Cerca materia</p>
-                <input onChange={(e) => {setMateria(e.target.value)}} className="dark:bg-zinc-950 bg-white dark:border-zinc-700 focus:dark:border-zinc-500 border-zinc-400 dark:text-white text-black border-2 rounded px-2 outline-none font-semibold text-base" type="text" />
+                <label for="search-class-input" className="dark:text-white text-black">Cerca materia</label>
+                <input id="search-class-input" onChange={(e) => {setMateria(e.target.value)}} className="dark:bg-zinc-950 bg-white dark:border-zinc-700 focus:dark:border-zinc-500 border-zinc-400 dark:text-white text-black border-2 rounded px-2 outline-none font-semibold text-base" type="text" />
             </div>
             <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 w-full mt-5">
                 {datiMaterie.map((item: any) => (
