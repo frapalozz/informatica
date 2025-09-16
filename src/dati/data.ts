fed16ea3c8fc72b7ed0e6ad3ae278c2c1db6e8f0
Francesco Palozzi <99217899+frapalozz@users.noreply.github.com>
2025-09-16 21:45:33 +0200

    Update data.ts

--- src/dati/data.ts
+++ src/dati/data.ts
@@ -15,9 +15,9 @@ export const dati =[
 
         webex: "https://unicam.webex.com/meet/alessandro.dellacorte",
 
-        extraName: ["Pass. Classroom", "Formulario", "Altre Prove"],
-        extra: ["POYLYUV", "Notion", "Notion"],
-        extraLink: ["https://classroom.google.com/u/7/c/NDAyNjAxMTEzMDE0", "https://francescopalozzi.notion.site/Formulario-Discreta-ae0a264c560142c0941f35e896fc7016?pvs=4", "https://francescopalozzi.notion.site/Altre-Prove-1854f1c8724e807ebcfdebc61a850242?pvs=4"],
+        extraName: ["Formulario", "Esercizi risolti", "Pass. Classroom", "Altre Prove"],
+        extra: ["Gumroad", "Gumroad", "POYLYUV", "Notion"],
+        extraLink: ["https://frapalozz.gumroad.com/l/formulario-matematica-discreta", "https://frapalozz.gumroad.com/l/matematica-discreta-esercizi", "https://classroom.google.com/u/7/c/NDAyNjAxMTEzMDE0", "https://francescopalozzi.notion.site/Altre-Prove-1854f1c8724e807ebcfdebc61a850242?pvs=4"],
         
         aggiornamento: "01/24/2025"
     },
